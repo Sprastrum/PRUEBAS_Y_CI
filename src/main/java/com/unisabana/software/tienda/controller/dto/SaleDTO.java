dc59e885b913e31c359d2dd015e15da886f3286f
Santiago Santos <78430563+Sprastrum@users.noreply.github.com>
2023-04-03 18:22:14 -0500

    Delete DTO database object functionality

--- src/main/java/com/unisabana/software/tienda/controller/dto/SaleDTO.java
+++ src/main/java/com/unisabana/software/tienda/controller/dto/SaleDTO.java
@@ -1,44 +1,13 @@
 package com.unisabana.software.tienda.controller.dto;
 
-import jakarta.persistence.*;
-import lombok.*;
-import org.hibernate.Hibernate;
+import lombok.Data;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
+import java.sql.Date;
 
-@Entity
-@Table(name = "SALE")
+@Data
 public class SaleDTO {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ID", length = 10) @Getter @Setter
     private Integer id;
-
-    @Column(name = "DATE_CREATED") @Getter @Setter
     private Date dateCreated;
-
-    @Column(name = "DOCUMENT_CLIENT", length = 10) @Getter @Setter
     private Integer documentClient;
-
-    @Column(name = "TOTAL_AMOUNT", length = 10) @Getter @Setter
     private Integer totalAmount;
-
-    @OneToMany
-    @ToString.Exclude
-    private List<SaleProductDTO> saleProductDTOList;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        SaleDTO saleDTO = (SaleDTO) o;
-        return id != null && Objects.equals(id, saleDTO.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 }
