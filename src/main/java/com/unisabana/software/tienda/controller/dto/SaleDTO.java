afac66987fe173f0cc819e58da55544f9e8fc5ee
Santiago Santos <78430563+Sprastrum@users.noreply.github.com>
2023-04-04 15:16:39 -0500

    Add list of SaleProductDTO attribute

--- src/main/java/com/unisabana/software/tienda/controller/dto/SaleDTO.java
+++ src/main/java/com/unisabana/software/tienda/controller/dto/SaleDTO.java
@@ -1,9 +1,12 @@
 package com.unisabana.software.tienda.controller.dto;
 
 import com.unisabana.software.tienda.model.Sale;
+import com.unisabana.software.tienda.model.SaleProduct;
 import lombok.Data;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 public class SaleDTO {
@@ -11,8 +14,15 @@ public class SaleDTO {
     private Date dateCreated;
     private Integer documentClient;
     private Integer totalAmount;
+    private List<SaleProductDTO> saleProductDTOS = new ArrayList<>();
 
     public Sale toModel() {
-        return new Sale(this.id, this.dateCreated, this.documentClient, this.totalAmount, null);
+        List<SaleProduct> saleProducts = new ArrayList<>();
+
+        for(SaleProductDTO s: saleProductDTOS) {
+            saleProducts.add(s.toModel());
+        }
+
+        return new Sale(this.id, this.dateCreated, this.documentClient, this.totalAmount, saleProducts);
     }
 }
--- src/main/java/com/unisabana/software/tienda/controller/dto/StockDTO.java
+++ src/main/java/com/unisabana/software/tienda/controller/dto/StockDTO.java
@@ -1,8 +1,13 @@
 package com.unisabana.software.tienda.controller.dto;
 
+import com.unisabana.software.tienda.model.Sale;
+import com.unisabana.software.tienda.model.SaleProduct;
+import com.unisabana.software.tienda.model.Stock;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Data
 public class StockDTO {
@@ -11,4 +16,15 @@ public class StockDTO {
     private String name;
     private Integer quantity;
     private Integer unitValue;
+    private List<SaleProductDTO> saleProductDTOS = new ArrayList<>();
+
+    public Stock toModel() {
+        List<SaleProduct> saleProducts = new ArrayList<>();
+
+        for(SaleProductDTO s: saleProductDTOS) {
+            saleProducts.add(s.toModel());
+        }
+
+        return new Stock(this.id, this.dateCreated, this.name, this.quantity, this.unitValue, saleProducts);
+    }
 }
